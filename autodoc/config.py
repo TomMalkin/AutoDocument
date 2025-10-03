b2cf2588f50686f708c7899859165005d77fbbd2
Thomas Malkin <tommalkin28@gmail.com>
2025-10-03 12:34:32 +1000

    update app config to match container defaults

--- autodoc/config.py
+++ autodoc/config.py
@@ -7,9 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-DB_PATH = os.getenv("DB_PATH", "/app/database/autodoc.db")
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+# defaults set to container setup, to minimise the docker-compose file.
+DB_PATH = os.getenv("DB_PATH", "/db_data/autodoc.db")
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 DOWNLOAD_DIRECTORY = Path(os.getenv("DOWNLOAD_DIRECTORY", "/download_dir"))
 UPLOAD_DIRECTORY = Path(os.getenv("UPLOAD_DIRECTORY", "/upload_dir"))
-
